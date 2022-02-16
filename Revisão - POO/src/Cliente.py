ca2ae4074732af7763fcb00188af6a92059fbd53
Lucas-Ribeir0 <lucassopessoal@gmail.com>
2022-02-16 20:39:44 -0300

    1

---
+++ Revisão - POO/src/Cliente.py
@@ -0,0 +1,8 @@
+class Cliente:
+    def __init__(self, cpf, nome, saldo) -> None:
+        self.cpf = cpf
+        self.nome = nome
+        self.saldo = saldo
+
+    if __name__ == "main":
+        
